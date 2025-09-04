f6a992193b9c55ef3c54ffb624c816993426c241
T-Ahish <ahishgameking@gmail.com>
2025-09-04 09:26:25 +0530

    Modify Function for Dynamic Grid based on User Choice

--- script/script.js
+++ script/script.js
@@ -4,16 +4,28 @@ const colorChangeHandler = (e) => {
   e.srcElement.style.backgroundColor = "rgba(0,0,0,1)";
 };
 
-for (let i = 0; i < 16; i++) {
-  for (let j = 0; j < 16; j++) {
-    let aDiv = document.createElement("div");
-    aDiv.classList.add("solid-border", "canvas-div");
-    containerCanvas.appendChild(aDiv);
-    aDiv.style.flex = `0 0 calc(100% / 16)`;
+const createDivs = (number = 16) => {
+  for (let i = 0; i < number; i++) {
+    for (let j = 0; j < number; j++) {
+      let aDiv = document.createElement("div");
+      aDiv.classList.add("solid-border", "canvas-div");
+      containerCanvas.appendChild(aDiv);
+      aDiv.style.flex = `0 0 calc(100% / ${number})`;
 
-    aDiv.addEventListener("mouseover", (e) => colorChangeHandler(e));
+      aDiv.addEventListener("mouseover", (e) => colorChangeHandler(e));
+    }
   }
-}
+};
+
+const gridCreationHandler = () => {
+  const userChoice = parseInt(prompt("choose the desired number for grid."));
+  if (userChoice > 100 || userChoice < 0) {
+    alert("Please choose the value between 0 and 100");
+    return;
+  }
+
+  createDivs(userChoice);
+};
 
 const clearButton = document.querySelector("button.clear-button");
 
@@ -24,3 +36,8 @@ const clearCanvasHandler = () => {
 };
 
 clearButton.addEventListener("click", clearCanvasHandler);
+
+const gridButton = document.querySelector("button.grid-button");
+gridButton.addEventListener("click", gridCreationHandler);
+
+createDivs();
